ffe8090a4d9f4e36c600d192e0603db67a27ba0d
Li Chao <leetschau@gmail.com>
2016-06-20 10:17:37 +0800

    exercise 1-2 over

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -1,26 +1,48 @@
-"""This file contains code for use with "Think Stats",
-by Allen B. Downey, available from greenteapress.com
 
-Copyright 2014 Allen B. Downey
-License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
-"""
+# coding: utf-8
 
-from __future__ import print_function
+# In[1]:
 
-import numpy as np
-import sys
-
-import nsfg
 import thinkstats2
 
-
-def main(script):
-    """Tests the functions in this module.
-
-    script: string script name
-    """
-    print('%s: All tests passed.' % script)
+dct_file='2002FemPreg.dct'
+dat_file='2002FemPreg.dat.gz'
 
 
-if __name__ == '__main__':
-    main(*sys.argv)
+# In[2]:
+
+dct = thinkstats2.ReadStataDct(dct_file)
+
+
+# In[4]:
+
+df = dct.ReadFixedWidth(dat_file, compression='gzip')
+
+
+# In[16]:
+
+print(df['pregnum'])
+
+
+# In[17]:
+
+df.caseid
+
+
+# In[13]:
+
+from collections import defaultdict
+
+d = defaultdict(list)
+
+
+# In[14]:
+
+for index, caseid in df.caseid.iteritems():
+    d[caseid].append(index)
+
+
+# In[15]:
+
+d
+
